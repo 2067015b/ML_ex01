54ba44334f6d86147abbee36a467f854c8e2a2ea
Monika Blstakova <nika.blstakova@gmail.com>
2018-03-09 19:52:23 +0000

    Best score so far, a mix of inv(x) and **2 features, using l1.

--- Regression/linear_regression_reg.py
+++ Regression/linear_regression_reg.py
@@ -13,7 +13,7 @@ y_train = np.loadtxt('y_train.csv', delimiter=',', skiprows=1)[:,1]
 
 X_train, X_test = normalize_data(X_train, X_test)
 
-def get_predictions_SGD(X_train, y_train, X_test, learning_rate=0.005, iterations=40000):
+def get_predictions_SGD_l2(X_train, y_train, X_test, learning_rate=0.005, iterations=27500):
     X_train = np.concatenate((np.ones((X_train.shape[0],1)),X_train),axis=1)
     X_test = np.concatenate((np.ones((X_test.shape[0],1)),X_test),axis=1)
 
@@ -24,7 +24,22 @@ def get_predictions_SGD(X_train, y_train, X_test, learning_rate=0.005, iteration
         prediction = X_train.dot(weights)
         error = y_train - prediction
         print("{} \terror: {}".format(i, np.mean(error.dot(X_train))))
-        weights += (learning_rate/sample_count) * (error.dot(X_train) + 0.01/sample_count * weights)
+        weights += (learning_rate/sample_count) * (error.dot(X_train) + 12/sample_count * weights)
+
+    return X_test.dot(weights)
+
+def get_predictions_SGD_l1(X_train, y_train, X_test, learning_rate=0.005, iterations=40500):
+    X_train = np.concatenate((np.ones((X_train.shape[0],1)),X_train),axis=1)
+    X_test = np.concatenate((np.ones((X_test.shape[0],1)),X_test),axis=1)
+
+    weights = np.ones(X_train.shape[1])
+    sample_count = X_train.shape[0]
+
+    for i in range(iterations):
+        prediction = X_train.dot(weights)
+        error = y_train - prediction
+        print("{} \terror: {}".format(i, np.mean(error.dot(X_train))))
+        weights += (learning_rate/sample_count) * (error.dot(X_train) + 12/sample_count * np.sign(weights))
 
     return X_test.dot(weights)
 
@@ -38,19 +53,20 @@ def get_predictions(X_train, y_train, X_test):
     return X_test.dot(weights)
 
 def score(X_val, y_val, X_train, y_train):
-    return 1 - sum((get_predictions_SGD(X_train,y_train, X_val) - y_val)**2) / sum((y_val - np.mean(y_val))**2)
+    return 1 - sum((get_predictions_SGD_l1(X_train,y_train, X_val) - y_val)**2) / sum((y_val - np.mean(y_val))**2)
 
 
 X_train = np.delete(X_train, [2,5], axis=1)
 X_test = np.delete(X_test, [2,5], axis=1)
 
+X_train = np.hstack((X_train,np.reshape(1/(X_train[:,0]+1), (X_train.shape[0], 1))))
+X_test = np.hstack((X_test,np.reshape((1/(X_test[:,0]+1)), (X_test.shape[0], 1))))
+
 
 dims = X_train.shape[1]
-for dimension in range(dims):
-    X_train = np.hstack((X_train,np.reshape((X_train[:,dimension]**2), (X_train.shape[0], 1))))
-    X_test = np.hstack((X_test,np.reshape((X_test[:,dimension]**2), (X_test.shape[0], 1))))
-    # X_train = np.hstack((X_train, np.reshape((X_train[:, dimension] ** 3), (X_train.shape[0], 1))))
-    # X_test = np.hstack((X_test, np.reshape((X_test[:, dimension] ** 3), (X_test.shape[0], 1))))
+for dimension in range(1,dims-1):
+    X_train = np.hstack((X_train,np.reshape(X_train[:,dimension]**2, (X_train.shape[0], 1))))
+    X_test = np.hstack((X_test,np.reshape((X_test[:,dimension])**2, (X_test.shape[0], 1))))
 
 
 # X_train, X_test = join_min_max(X_train, X_test)
@@ -58,9 +74,10 @@ for dimension in range(dims):
 
 X_temp, y_temp, X_val, Y_val = split_set(X_train, y_train, 40)
 
+
 # print(score(X_val, Y_val, X_temp, y_temp))
 
-y_pred = get_predictions_SGD(X_train,y_train,X_test)
+y_pred = get_predictions_SGD_l1(X_train,y_train,X_test)
 
 # Fit model and predict test values
 # y_pred = np.random.randint(y_train.min(), y_train.max(), X_test.shape[0])
